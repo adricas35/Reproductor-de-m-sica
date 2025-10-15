ff185302d74a588fc5200aeda84f25bd6698d1bf
Adriana Castillo <adricasra1993@gmail.com>
2025-10-15 09:57:41 -0500

    Agregue comentarios al codigo

--- js/main.js
+++ js/main.js
@@ -57,27 +57,29 @@ btnSiguiente.addEventListener("click", () => {
         // Multiplicar por la longitud de la lista para obtener un rango de 0 hasta (longitud del arreglo)
         // Math.floor() redondea hacia abajo para obtener un índice válido
         const indiceAleatorio = Math.floor(Math.random() * (canciones.length));
+        // Se asigna el indice aleatorio al indice actual para visualizar la cancion
         indiceActual = indiceAleatorio;
     } else {
         // Esto lo ponemos por si en indice llega al ultimo retorne al primero
         if (indiceActual === canciones.length - 1) [
             indiceActual = 0
         ]
+        // Si no entonces el indice actual aumenta al siguiente valor
         else {
             indiceActual++
         }
     }
     // Retornamos el valor de la funcion inical
     mostraCanciones(indiceActual);
-    // REPRODUCIMOS LA CANCIÓN DESPUES DE ACTUALIZAR EL ÍNDICE O SI NO SE REPRODUCE LA CANCIÓN ANTERIOR EN EL ARREGLO AL CAMBIAR AL BOTÓN SEGUIENTE
+    // Reproducimos la canción despues de actualizar el índice o si no se reproduce la canción anterior en el arreglo al cambiar al botón seguiente
     const cancion = canciones[indiceActual]
-    // CAMBIAMOS LA CANCIÓN
+    // Cambiamos la canción
     audio.setAttribute('src', cancion.cancion);
-
+    // Se reproduce la cancion
     audio.play()
-
+    // Se cambia el icono de play al icono de pausa
     btnPlayPause.innerHTML = '<i class="bi bi-pause-fill"></i>'
-    // MANTENEMOS EL BOOLEANO EN TRUE PARA QUE NO IMPORTE SI LA CANCIÓN ESTÁ PAUSADA O NO, SE REPRODUZCA SIEMPRE LA SIGUIENTE AL PRESIONAR EL BOTÓN DE NEXT
+    // Mantenemos el booleano en true para que no importe si la canción está pausada o no, se reproduzca siempre la siguiente al presionar el botón de next
     isPlaying = true
 })
 
@@ -88,41 +90,41 @@ btnAnterior.addEventListener("click", () => {
         // Multiplicar por la longitud de la lista para obtener un rango de 0 hasta (longitud - 1)
         // Math.floor() redondea hacia abajo para obtener un índice válido
         const indiceAleatorio = Math.floor(Math.random() * (canciones.length + 1));
-        // GUARDAMOS EN LA VARIABLE INDICEACTUAL EL VALOR ALEATORIO QUE SE GUARDA EN LA VARIABLE INDICE ALEATORIO
+        // Guardamos en la variable indiceactual el valor aleatorio que se guarda en la variable indice aleatorio
         indiceActual = indiceAleatorio;
     } else {
         // Esto lo ponemos por si el indice esta en el primero y retorne al ultimo 
         if (indiceActual === 0) [
-
             indiceActual = canciones.length - 1
         ]
+        // Si no entonces el indice actual disminuye al anterior valor
         else {
             indiceActual--;
         }
     }
     mostraCanciones(indiceActual);
-    // REPRODUCIMOS LA CANCIÓN DESPUES DE ACTUALIZAR EL ÍNDICE O SI NO SE REPRODUCE LA CANCIÓN ANTERIOR EN EL ARREGLO AL CAMBIAR AL BOTÓN ANTERIOR
+    // Reproducimos la canción despues de actualizar el índice o si no se reproduce la canción anterior en el arreglo al cambiar al botón anterior
     const cancion = canciones[indiceActual]
-    // CAMBIAMOS LA CANCIÓN
+    // Cambiamos la canción
     audio.setAttribute('src', cancion.cancion);
-
+    // Se reproduce la cancion
     audio.play()
-
+    // Se cambia el icono de play al icono de pausa
     btnPlayPause.innerHTML = '<i class="bi bi-pause-fill"></i>'
-    // MANTENEMOS EL BOOLEANO EN TRUE PARA QUE NO IMPORTE SI LA CANCIÓN ESTÁ PAUSADA O NO, SE REPRODUZCA SIEMPRE LA SIGUIENTE AL PRESIONAR EL BOTÓN DE NEXT
+    // Mantenemos el booleano en true para que no importe si la canción está pausada o no, se reproduzca siempre la siguiente al presionar el botón de next
     isPlaying = true
 })
 
 
 // Se agregar el evento "click" al botón para pausar y reproducir la cación 
 btnPlayPause.addEventListener('click', () => {
-
+    // Si la variable isPlaying esta en true pausamos la musica y cambiamos el icono de play a pausa
     if (isPlaying) {
-        // Si está sonando, pausa la reproducción y cambia el icono del botón
+        // Si está sonando, pausa la reproducción y cambia el icono del botón a play
         audio.pause();
         btnPlayPause.innerHTML = '<i class="bi bi-play-fill"></i>';
     } else {
-        // Si no está sonando, reproduce y cambia el icono del botón
+        // Si no está sonando, reproduce y cambia el icono del botón a pausa
         audio.play();
         btnPlayPause.innerHTML = '<i class="bi bi-pause-fill"></i>';
     }
@@ -131,22 +133,22 @@ btnPlayPause.addEventListener('click', () => {
 });
 
 
-// SINCRONIZAR LA BARRA DE PROGRESO CON EL AUDIO DE LA CANCIÓN
-// timeupdate ES UN EVENTO QUE SE DISPARA CUANDO SE ESTÁ REPRODUCIENDO LA CANCIÓN ES PROPIO DE LA ETIQUETA AUDIO
+// Sincronizar la barra de progreso con el audio de la canción
+// Timeupdate es un evento que se dispara cuando se está reproduciendo la canción es propio de la etiqueta audio
 audio.addEventListener('timeupdate', () => {
-    // 1. OBTENEMOS CUANTO TIEMPO LLEVA REPRODUCIDO EL AUDIO
+    // Obtenemos cuanto tiempo lleva reproducido el audio
     audio.currentTime;
-    // 2. OBTENEMOS LA DURACIÓN TOTAL DE LA CANCIÓN
+    // Obtenemos la duración total de la canción
     audio.duration;
-    // 3. CALCULAMOS EL PORCENTAJE DE PROGRESO, DIVIDIENDO EL TIEMPO ACTUAL POR LA DURACIÓN TOTAL DE LA CANCIÓN Y MULTIPLICANDO POR 100 Y LO GUARDAMOS EN UNA VARIABLE
+    // Calculamos el porcentaje de progreso, dividiendo el tiempo actual por la duración total de la canción y multiplicando por 100 y lo guardamos en una variable
     let porcentaje = 0;
-    // 4. ACÁ DECIMOS QUE SI LA DURACIÓN TOTAL DE LA CANCIÓN ES 0 ENTONCES SE EJECUTE LA OPERACIÓN PARA ACTUALIZAR LA BARRA DE PROGRESO Y EL MOVIMIENTO DE LA BOLITA LO MULTIPLICAMOS ENTRE 100 PARA QUE SE VUELVA PORCENTAJE.
+    // Acá decimos que si la duración total de la canción es 0 entonces se ejecute la operación para actualizar la barra de progreso y el movimiento de la bolita lo multiplicamos entre 100 para que se vuelva porcentaje.
     if (audio.duration) {
         porcentaje = audio.currentTime / audio.duration * 100
     }
-    // 5. VAMOS A ACTUALIZAR LA BARRA AMARILLA JUNTO CON LA BOLITA, IMPORTANTE AGREGAR EL % PARA QUE EL GRADIENT DE LA BARRA LO INTERPRETE CORRECTAMENTE
+    // Vamos a actualizar la barra amarilla junto con la bolita, importante agregar el % para que el gradient de la barra lo interprete correctamente
     barraProgress.style.setProperty('--value', `${porcentaje}%`);
-    // 6. LE ASIGNAMOS EL VALOR GUARDADO EN PORCENTAJE A LA BARRA DE PROGRESO
+    // Le asignamos el valor guardado en porcentaje a la barra de progreso
     barraProgress.value = porcentaje
 
 
@@ -176,72 +178,81 @@ audio.addEventListener('timeupdate', () => {
 })
 
 
-// SE CREA LA FUNCION PARA QUE AL MOVER LA BOLITA DE LA BARRA LA CANCIÓN SE ACTUALICE AL PUNTO EXACTO
+// Se crea la funcion para que al mover la bolita de la barra la canción se actualice al punto exacto
 barraProgress.addEventListener('input', () => {
-    // TOMAMOS EL VALOR ACTUAL DE LA BARRA CON EL .VALUE QUE ES UN INPUT
+    // Tomamos el valor actual de la barra con el .value que es un input
     barraProgress.value;
-    // DECLARAMOS UNA VARIABLE INICIALIZADA EN 0 PARA GUARDAR EL RESULTADO DE MULTIPLICAR Y EL VALOR ACTUAL DE LA BARRA POR LA DURACIÓN REAL DE LA CANCIÓN DIVIDO POR 100 MULTIPLICAMOS Y DIVIMOS ENTRE 100 PARA PASAR UN VALOR PORCENTUAL A SEGUNDOS REALES
+    // Declaramos una variable inicializada en 0 para guardar el resultado de multiplicar y el valor actual de la barra por la duración real de la canción divido por 100 multiplicamos y divimos entre 100 para pasar un valor porcentual a segundos reales
     let nuevoTiempo = 0
-    // GUARDAMOS EL RESULTADO EN LA VARIABLE
+    // Guardamos el resultado en la variable
     nuevoTiempo = (barraProgress.value * audio.duration) / 100;
-    // GUARDAMOS EN EL TIEMPO TRANSCURRIDO LA VARIABLE CON EL RESULTADO PARA ASÍ TERMINAR LA FUNCIÓN
+    // Guardamos en el tiempo transcurrido la variable con el resultado para así terminar la función
     audio.currentTime = nuevoTiempo;
 })
 
-// se crea evento click en el boton de la lista para visualizar y ocultar la lista de canciones
+
+// Se crea evento click en el boton de la lista para visualizar y ocultar la lista de canciones
 btnList.addEventListener('click', () => {
     const containerList = document.querySelector('.cont-lista');
     containerList.classList.add('lista-visible')
 });
 
+
+// Se crea el evento click en el boton regresar para ocultar la lista de canciones
 btnRegreso.addEventListener('click', ()=>{
     const containerList = document.querySelector('.cont-lista');
     containerList.classList.remove('lista-visible');
 })
 
 
+// Se crea la funcion para mostrar la lista de canciones 
 function mostrarCancionesEnLista() {
+// Se recorre el arreglo de la lista de canciones 
     canciones.forEach((cancion, indice) => {
+        // Se crea el div contenedor de la informacion de la cancion
         const fila = document.createElement('div');
         fila.classList.add('fila');
-
+        // Se crea el contenedor de la imagen de la cancion
         const conImg = document.createElement('div');
         conImg.classList.add('cont-img');
-
+        // Se crea el elemento img
         const img = document.createElement('img');
+        // Se le agrega la ruta de la caratula de la cancion 
         img.setAttribute('src', cancion.caratula);
+        // Se le agrega el atributo alt con el nombre de la cancion
         img.setAttribute('alt', cancion.nombre);
 
+        // Se crea el contenedor del detalle de la cancion 
         const contInfo = document.createElement('div');
         contInfo.classList.add('cont-info');
-
+        // Se crea la etiqueta con el nombre de la cancion
         const h3 = document.createElement('h3');
         h3.textContent = cancion.nombre;
-
+        // Se crea la etiqueta con el nombre del artista
         const h4 = document.createElement('h4');
         h4.textContent = cancion.artista;
-
+        // Se crea el boton para reproducir la cancion
         const boton = document.createElement('button');
         boton.innerHTML = '<i class="bi bi-play-fill"></i>'
-
+        // Se crea el evento click para reproducir la cancion 
         boton.addEventListener('click', ()=>{
+            // Se le asigna el indice de esta cancion al indice actual
             indiceActual= indice
-
+            // Se muestra la cancion seleccionada
             mostraCanciones(indiceActual);
-
+            // Se asigna la cancion para reproducir 
             audio.setAttribute('src', cancion.cancion);
-
+            // Se reproduce la cancion
             audio.play();
-
+            // Se cambia el icono del boton de play a pausa
             boton.innerHTML = '<i class="bi bi-pause-fill"></i>'
-
+            // Se cambia el icono del boton en el reproducto de play a pausa
             btnPlayPause.innerHTML = '<i class="bi bi-pause-fill"></i>'
-
+            // Se cambia la variable isPlaying a true
             isPlaying= true
-
+            // Se oculta la lista de cancion para mostrar el reproductor
            const containerList = document.querySelector(".cont-lista");
            containerList.classList.remove('lista-visible');
-
         })
 
 
@@ -271,9 +282,10 @@ document.addEventListener("DOMContentLoaded", () => {
             // Esto llama una funcion que va a mostrar los datos en la tabla
             mostraCanciones(indiceActual)
 
-            // INICIALIZAMOS EL SRC AQUÍ PARA PODER USARLO EN EL BOTÓN DE REPRODUCIR.
+            // Inicializamos el src aquí para poder usarlo en el botón de reproducir.
             audio.setAttribute('src', canciones[indiceActual].cancion)
 
+            // Se llama la funcion que va a mostrar la lista de canciones 
             mostrarCancionesEnLista()
 
         })
